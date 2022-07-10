d97455e1659d9318767fd827e5592b669a389418
ConstantinP-07 <constantin.a.paech@gmail.com>
2022-07-10 18:02:39 +0200

    First Working Controller
    Implemented the First working Controller.
    Including:
    Walking
    Looking Around and Smoothing
    Missing: Look lock and Crouching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private Rigidbody rb;
+    public float movementSpeed;
+    public float mouseSensitivity;
+    public float lerpSpeed;
+    public float movementLerpSpeed;
+
+    private float xRot = 0f;
+    private float yRot = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horiz = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        xRot += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        yRot += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         
-        Vector3 horizV = transform.right * horiz;
-        Vector3 vertV = transform.forward * vert;
 
-        Vector3 movement = horizV + vertV;
-        movement = movement.normalized;
+        Vector3 movement = transform.right * horizontal + transform.forward * vertical;
+        movement = movement.normalized * movementSpeed;
+        movement = new Vector3(movement.x, 0f, movement.z);
 
-        transform.position += movement;
+        Camera.main.transform.localRotation = Quaternion.Euler(Mathf.LerpAngle(Camera.main.transform.rotation.eulerAngles.x, -yRot, Mathf.Clamp01(lerpSpeed * Time.deltaTime)), 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, Mathf.LerpAngle(transform.rotation.eulerAngles.y, xRot, lerpSpeed * Time.deltaTime), 0f);
+
+        rb.velocity = Vector3.Lerp(rb.velocity, movement, Time.deltaTime * movementLerpSpeed);
     }
 }
